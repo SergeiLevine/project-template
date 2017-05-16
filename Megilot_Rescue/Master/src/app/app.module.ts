576857f4c69b22d0afe7f186b14e714880fe64c8
kourinal <alexkourin@gmail.com>
2017-05-16 10:48:46 +0300

    single polyline

--- Megilot_Rescue/Master/src/app/app.module.ts
+++ Megilot_Rescue/Master/src/app/app.module.ts
@@ -3,25 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { NavItemComponent } from './nav-item/nav-item.component';
 import { MapComponent } from './map/map.component';
-import { AgmCoreModule } from 'angular2-google-maps/core';
 import { MarkerComponent } from './marker/marker.component';
 import { PolylineComponent } from './polyline/polyline.component';
 import { PolygonComponent } from './polygon/polygon.component';
-import {MarkerService} from './map/marker.service';
-import { PathComponent } from './path/path.component';
-import { MainMenuComponent } from './main-menu/main-menu.component';
-import { EventMenuComponent } from './event-menu/event-menu.component';
-import {ImageUploadModule} from 'angular2-image-upload';
-import { AuthGuard } from './_guards/auth.guard';
-import { AlertService, AuthenticationService, UserService } from './_services/index';
-import { LoginComponent } from './login';
-import { AppConfig } from './app.config';
 import { AlertComponent } from './alert/alert.component';
 import { HomeComponent } from './home/home.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { EventMenuComponent } from './event-menu/event-menu.component';
+import { LoginComponent } from './login';
+import { PathComponent } from './path/path.component';
+
+import {MarkerService} from './map/marker.service';
+import { AlertService, AuthenticationService, UserService } from './_services/index';
+
+import { AuthGuard } from './_guards/auth.guard';
+
+import { AgmCoreModule } from 'angular2-google-maps/core';
+import {ImageUploadModule} from 'angular2-image-upload';
+
+import { AppConfig } from './app.config';
+
 
 const appRoutes: Routes=[
  // { path: '', component: HomeComponent, canActivate: [AuthGuard] },
--- Megilot_Rescue/Master/src/app/map/map.component.ts
+++ Megilot_Rescue/Master/src/app/map/map.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit,Injectable,Pipe, PipeTransform } from '@angular/core';
-import {Observable} from 'rxjs/Observable';
-import {AnonymousSubscription} from "rxjs/Subscription";
-import {Http, Response} from '@angular/http';
-import { SebmGoogleMap, SebmGoogleMapPolygon, LatLngLiteral,SebmGoogleMapPolyline, SebmGoogleMapPolylinePoint,} 
-from 'angular2-google-maps/core';
+import { Component, OnInit, Injectable, Pipe, PipeTransform } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AnonymousSubscription } from "rxjs/Subscription";
+import { Http, Response } from '@angular/http';
+import { SebmGoogleMap, SebmGoogleMapPolygon, LatLngLiteral, SebmGoogleMapPolyline, SebmGoogleMapPolylinePoint, }
+  from 'angular2-google-maps/core';
 import 'rxjs/Rx';
-import {MarkerService} from './marker.service'
+import { MarkerService } from './marker.service'
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -17,130 +17,135 @@ export class MapComponent implements OnInit {
   title: string = 'My first angular2-google-maps project';
   lat: number = 31.7683;
   lng: number = 35.2137;
-  areaCount: number=0;
-  drawble:boolean=false;
+  areaCount: number = 0;
+  drawble: boolean = false;
 
 
-  realData=[];
-  realCords=[];
-  url='http://5b889434.ngrok.io/send_data.php';
-  pathArray=[[{user:'A',lat:30,lng:30},{user:'A',lat:31,lng:31},{user:'A',lat:32,lng:32}],
-  [{user:'B',lat:33,lng:33},{user:'B',lat:34,lng:34},{user:'B',lat:34,lng:34}]];
-  
+  realData = [];
+  realCords = [];
+  url = 'http://5b889434.ngrok.io/send_data.php';
+  pathArray = [
+    [
+      { user: 'A', lat: 30, lng: 30 }, 
+      { user: 'A', lat: 31, lng: 31 }, 
+      { user: 'A', lat: 32, lng: 32 }
+    ],
+    [{ user: 'B', lat: 33, lng: 33 }, { user: 'B', lat: 34, lng: 34 }, { user: 'B', lat: 34, lng: 34 }]];
+
   /*paths: Array<LatLngLiteral> = [
     
    
   ]*/
-  paths=[];
-  
-   mapClicked(e){
-     if (this.drawble==false){
-       return;
-     }
-     //var temp=this.paths;
-     var temp=[];
-     /*for (let i of this.paths){
-       temp.push(i);
-     }*/
+  paths = [];
+
+  mapClicked(e) {
+    if (this.drawble == false) {
+      return;
+    }
+    //var temp=this.paths;
+    var temp = [];
+    /*for (let i of this.paths){
+      temp.push(i);
+    }*/
     console.log(this.paths.length);
-    if (this.paths.length>0 && this.paths[this.areaCount-1]!=undefined){
-       console.log(this.paths[this.areaCount-1]);
-       for (let i of this.paths[this.areaCount-1]){
-        
+    if (this.paths.length > 0 && this.paths[this.areaCount - 1] != undefined) {
+      console.log(this.paths[this.areaCount - 1]);
+      for (let i of this.paths[this.areaCount - 1]) {
         temp.push(i);
-       }
-    }  
-    temp.push({lat:parseFloat(e.coords.lat),lng:parseFloat(e.coords.lng)});
+      }
+    }
+    temp.push({ lat: parseFloat(e.coords.lat), lng: parseFloat(e.coords.lng) });
     // this.paths=temp;
-    this.paths[this.areaCount-1]=temp;
+    this.paths[this.areaCount - 1] = temp;
     //console.log(this.paths);
-   }
-  createPath(k){
-    
-    var newpath=[];
-    var j=0;
-    for (var i=0;i<k.length;i++){
+  }
+  createPath(k) {
+
+    var newpath = [];
+    var j = 0;
+    for (var i = 0; i < k.length; i++) {
       var n;
-      if (j==0){
-        n=({lat1:k[j].lat,lat2:k[j].lat,lng1:k[j].lng,lng2:k[j].lng});
+      if (j == 0) {
+        n = ({ lat1: k[j].lat, lat2: k[j].lat, lng1: k[j].lng, lng2: k[j].lng });
       }
-      else{
-        n=({lat1:k[j-1].lat,lat2:k[j].lat,lng1:k[j-1].lng,lng2:k[j].lng});
+      else {
+        n = ({ lat1: k[j - 1].lat, lat2: k[j].lat, lng1: k[j - 1].lng, lng2: k[j].lng });
       }
-     
-      newpath[j]=n;
-      
+
+      newpath[j] = n;
+
       j++;
 
     }
     console.log(newpath);
     return newpath;
   }
-  
-  getLat(k){
-    return (k[k.length-1].lat);
-  } 
-  getLng(k){
-    return (k[k.length-1].lng);
-  } 
-   getUser(k){
-    return (k[k.length-1].user);
-  } 
-  drawPolygon(n,l,t){
+
+  getLat(k) {
+    return (k[k.length - 1].lat);
+  }
+  getLng(k) {
+    return (k[k.length - 1].lng);
+  }
+  getUser(k) {
+    return (k[k.length - 1].user);
+  }
+  drawPolygon(n, l, t) {
     //var temp={user:String,latitude:String,longitude:String};
-    if (this.drawble==false)
+    if (this.drawble == false)
       this.areaCount++;
-    this.drawble=!this.drawble;
+    this.drawble = !this.drawble;
   }
-  toMarker(r){
-    var marker={
-      user:r.user,
-      latitude:r.latitude,
-      longitude:r.longitude};
-      return marker;
+  toMarker(r) {
+    var marker = {
+      user: r.user,
+      latitude: r.latitude,
+      longitude: r.longitude
+    };
+    return marker;
 
   }
 
-  constructor(private http:Http) { 
-    for (let k of this.realData){
-      k.latitude=parseFloat(k.latitude);
-      k.longitude=parseFloat(k.longitude);
+  constructor(private http: Http) {
+    for (let k of this.realData) {
+      k.latitude = parseFloat(k.latitude);
+      k.longitude = parseFloat(k.longitude);
       this.realCords.push(k);
     }
-    this.realData=this.realCords;
-    
+    this.realData = this.realCords;
+
 
   }
-  
+
   ngOnInit() {
     this.getData();
-    
+
   }
   _postservice;
   outputs;
-  getData(){
-      this.http.get(this.url).subscribe(res =>{ 
+  getData() {
+    this.http.get(this.url).subscribe(res => {
       this.realData = res.json();
       //this.refreshData();
     });
-      
-    }
-   
-    get data(){
-      //console.log('in data')
-      var i=0;
-      
-      for(let d of this.realData){ 
-        
-        d.longitude = parseFloat(d.longitude);
-        d.latitude = parseFloat(d.latitude);
-        //this.pathArray.push({lat:d.latitude,lng:d.longitude});
-       
-      }
 
-      
-      
-      return this.realData;
+  }
+
+  get data() {
+    //console.log('in data')
+    var i = 0;
+
+    for (let d of this.realData) {
+
+      d.longitude = parseFloat(d.longitude);
+      d.latitude = parseFloat(d.latitude);
+      //this.pathArray.push({lat:d.latitude,lng:d.longitude});
+
     }
+
+
+
+    return this.realData;
+  }
 
 }
