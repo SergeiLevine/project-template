4897d809b72c06b4170041d8e3239de86b2d8130
Sergei Levine <Sergei Levine>
2017-05-20 22:51:54 +0300

    sending data to alexey 0.0.1

--- Megilot_Rescue/Server/send_data.php
+++ Megilot_Rescue/Server/send_data.php
@@ -1,5 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+include 'head.php';
 
 $dbhost = "localhost";
 $dbuser = "root";
@@ -10,20 +11,58 @@ $database = "megilot_database";
 
 $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$database) or die($dberror1);
 
-$query = "SELECT * FROM coordinates";
+$event_id = read_from_ses('event_id');
+//getting all the user_id of users participating in the event
+$query_users = "SELECT user_id FROM event__$event_id";
 
-$fetch_sql_query = mysqli_query($conn,$query)  or die ("Could not find user");
 
-	while($row = mysqli_fetch_assoc($fetch_sql_query))
+$run_query=mysqli_query($conn,$query_users);
+
+$query_num_of_participants=mysqli_num_rows($run_query);
+
+	if($query_num_of_participants==0)
 	{
-	echo $row['latitude'];
-	echo " ";
-	echo $row['longitude']."<br>";
+		echo 'No participants yet';
 	}
+	else
+	{
 
-	
+		$i = 1;
+		$query_users_coordinates = "";
+		while($participants = mysqli_fetch_assoc($run_query))
+		{
+			$participants = $participants['user_id'];
+			$query_users_coordinates .= "SELECT user_name, latitude, longitude
+				FROM event__$event_id__$participants";
+			if($i < $query_num_of_participants)
+			{
+				
+				$query_users_coordinates .= " UNION ALL ";	
+			}
+			$i++;
+		}
+		
+		//getting all the usernames with their coordinates
+		$run_query=mysqli_query($conn,$query_users_coordinates);
+		$query_num_rows=mysqli_num_rows($run_query);
+		
+		//small arrays of users that will go into one big array for ease of json sending
+		$data = array(array());	
+		$j = -1;
+		$temp = "";
+		while($participants = mysqli_fetch_assoc($run_query))
+		{
+			if($temp != $participants)
+				$j++;
+			
+			array_push($data[$j],array('user_name' => $participants['user_name'], 'latitude' => $participants['latitude'], 'longitude' => $participants['longitude']));
+			
 
-
-
+		}
+		
+		echo json_encode($data);
+		
+		
+	}
 
 ?>
